c92fed204e40b276020df648015bfce9c7aee411
itxSaaad <saadstudent.cs@gmail.com>
2023-10-29 16:50:06 +0500

    Added: Hero Section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,6 @@ import { motion } from 'framer-motion';
 import styles from '@/styles';
 import { navVariants } from '@/utils/motion';
 
-import React from 'react';
-
 function Navbar() {
   return (
     <motion.nav
