a949fca71f4268beaf635135a512a4c0267cba8d
itxSaaad <saadstudent.cs@gmail.com>
2023-11-11 20:22:32 +0500

    Added: FeedBack Section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 
 import { InsightCard, TitleText, TypingText } from '@/components';
-import styles from '@/styles';
 import { insights } from '@/constants';
-import { fadeIn, staggerContainer } from '@/utils/motion';
+import styles from '@/styles';
+import { staggerContainer } from '@/utils/motion';
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
